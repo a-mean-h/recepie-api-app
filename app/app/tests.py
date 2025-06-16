d1d1c2946cfbd9977228d452e02307b074b7a4e7
amin-hendian <amin.hendian@remotereps.com>
2025-06-15 20:35:39 -0400

    configuring database

---
+++ app/app/tests.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+
+from app import calc
+class CalcTests(SimpleTestCase):
+    def test_add_number(self):
+        res = calc.add(8,1)
+        self.assertEqual(res, 9)
+# ---
+# name: Checks
